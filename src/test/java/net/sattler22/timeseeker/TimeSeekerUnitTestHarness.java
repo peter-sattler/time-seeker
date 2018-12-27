ef7d4f2e127d39dc02cbe54902986e7c41fdbe98
Peter Sattler <peter-sattler@users.noreply.github.com>
2018-12-27 09:17:11 -0500

    Update TimeSeekerUnitTestHarness.java

--- src/test/java/net/sattler22/timeseeker/TimeSeekerUnitTestHarness.java
+++ src/test/java/net/sattler22/timeseeker/TimeSeekerUnitTestHarness.java
@@ -18,62 +18,62 @@ public final class TimeSeekerUnitTestHarness {
 
     @Test
     public void minimumTimeTestCase() throws TimeFittingException {
-        assertValieSolution(new int[] { 0, 0, 0, 0, 0, 0 }, "00:00:00");
+        assertValidSolution(new int[] { 0, 0, 0, 0, 0, 0 }, "00:00:00");
     }
 
     @Test
     public void maximumTimeTestCase() throws TimeFittingException {
-        assertValieSolution(new int[] { 2, 9, 5, 3, 5, 9 }, "23:59:59");
+        assertValidSolution(new int[] { 2, 9, 5, 3, 5, 9 }, "23:59:59");
     }
 
     @Test
     public void happyPathTestCase1() throws TimeFittingException {
-        assertValieSolution(new int[] { 0, 0, 0, 0, 0, 1 }, "00:00:01");
+        assertValidSolution(new int[] { 0, 0, 0, 0, 0, 1 }, "00:00:01");
     }
 
     @Test
     public void happyPathTestCase2() throws TimeFittingException {
-        assertValieSolution(new int[] { 1, 9, 9, 9, 1, 1 }, "19:19:19");
+        assertValidSolution(new int[] { 1, 9, 9, 9, 1, 1 }, "19:19:19");
     }
 
     @Test
     public void happyPathTestCase3() throws TimeFittingException {
-        assertValieSolution(new int[] { 1, 6, 2, 3, 4, 8 }, "12:36:48");
+        assertValidSolution(new int[] { 1, 6, 2, 3, 4, 8 }, "12:36:48");
     }
 
     @Test
     public void happyPathTestCase4() throws TimeFittingException {
-        assertValieSolution(new int[] { 0, 7, 0, 8, 0, 9 }, "07:08:09");
+        assertValidSolution(new int[] { 0, 7, 0, 8, 0, 9 }, "07:08:09");
     }
 
     @Test
     public void happyPathTestCase5() throws TimeFittingException {
-        assertValieSolution(new int[] { 1, 2, 3, 4, 5, 6 }, "12:34:56");
+        assertValidSolution(new int[] { 1, 2, 3, 4, 5, 6 }, "12:34:56");
     }
 
     @Test
     public void happyPathTestCase6() throws TimeFittingException {
-        assertValieSolution(new int[] { 0, 0, 2, 4, 0, 0 }, "00:00:24");
+        assertValidSolution(new int[] { 0, 0, 2, 4, 0, 0 }, "00:00:24");
     }
 
     @Test
     public void happyPathTestCase7() throws TimeFittingException {
-        assertValieSolution(new int[] { 1, 6, 2, 7, 3, 8 }, "16:27:38");
+        assertValidSolution(new int[] { 1, 6, 2, 7, 3, 8 }, "16:27:38");
     }
 
     @Test
     public void happyPathTestCase8() throws TimeFittingException {
-        assertValieSolution(new int[] { 2, 3, 4, 4, 4, 4 }, "23:44:44");
+        assertValidSolution(new int[] { 2, 3, 4, 4, 4, 4 }, "23:44:44");
     }
 
     @Test
     public void happyPathTestCase9() throws TimeFittingException {
-        assertValieSolution(new int[] { 2, 0, 0, 0, 2, 2 }, "00:02:22");
+        assertValidSolution(new int[] { 2, 0, 0, 0, 2, 2 }, "00:02:22");
     }
 
     @Test
     public void happyPathTestCase10() throws TimeFittingException {
-        assertValieSolution(new int[] { 2, 4, 0, 0, 0, 0 }, "00:00:24");
+        assertValidSolution(new int[] { 2, 4, 0, 0, 0, 0 }, "00:00:24");
     }
 
     @Test(expected = TimeFittingException.class)
@@ -104,7 +104,7 @@ public final class TimeSeekerUnitTestHarness {
     /**
      * Valid solution check
      */
-    private void assertValieSolution(int[] values, String expected) throws TimeFittingException {
+    private void assertValidSolution(int[] values, String expected) throws TimeFittingException {
         assertEquals(expected, findEarliestImpl(values));
     }
 
