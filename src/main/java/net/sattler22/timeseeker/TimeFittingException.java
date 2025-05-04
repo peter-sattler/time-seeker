8adcf9640bea6a32c05c13dca5a84a6c9304011c
Peter Sattler <peter@sattler22.net>
2025-05-03 23:16:13 -0400

    Upgraded to Java 24

--- src/main/java/net/sattler22/timeseeker/TimeFittingException.java
+++ src/main/java/net/sattler22/timeseeker/TimeFittingException.java
@@ -1,15 +1,1 @@
-package net.sattler22.timeseeker;
-
-/**
- * Time Seeker Time Fitting Exception
- *
- * @author Pete Sattler
- * @version December 2018 (brute-force)
- * @version February 2022 (rewrite)
- */
-final class TimeFittingException extends IllegalStateException {
-
-    TimeFittingException(String message) {
-        super(message);
-    }
-}
+package net.sattler22.timeseeker;/** * Time Seeker Time Fitting Exception * * @author Pete Sattler * @version December 2018 (brute-force) * @version February 2022 (rewrite) */final class TimeFittingException extends IllegalStateException {    TimeFittingException(String message) {        super(message);    }}
